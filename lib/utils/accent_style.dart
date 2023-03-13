14e2ef57d0e53f1a769ea841dcfa9cdd8abdc08f
Abinnnn <mr.adhityabayu@gmail.com>
2023-03-13 22:42:22 +0700

    app styles

---
+++ lib/utils/accent_style.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color(0xFF3EDEAE);
+
+class Styles {
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
+
+  static TextStyle textStyle =
+      TextStyle(fontSize: 26, color: textColor, fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle1 =
+      TextStyle(fontSize: 24, color: textColor, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle2 =
+      TextStyle(fontSize: 14, color: textColor, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle3 =
+      TextStyle(fontSize: 12, fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle4 = TextStyle(
+      fontSize: 14,
+      color: Colors.grey.shade500,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 3);
+}
