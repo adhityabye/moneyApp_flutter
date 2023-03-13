d76234c8c71f9926482fdc90fc9c57c20c95ebd2
Abinnnn <mr.adhityabayu@gmail.com>
2023-03-13 22:42:36 +0700

    app routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class AppRoutes {
+  static List<GetPage> routes() => [
+        // GetPage(
+        //   name: "/",
+        //   page: () => const SplashScreen(),
+        // ),
+      ];
+}
